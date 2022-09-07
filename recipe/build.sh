460ac8fa42960f28d294d3fe09a4bce281318d6f
Blaise Thompson <blaise@untzag.com>
2022-09-06 20:42:23 -0500

    remove cp for linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,6 @@ if [ $OSX_ARCH ]
 then
     cp "$RECIPE_DIR/menu-osx.json" "$PREFIX/Menu"
     cp "$RECIPE_DIR/app.icns" "$PREFIX/Menu"
-else
-    cp "$RECIPE_DIR/menu-linux.json" "$PREFIX/Menu"
-    cp "$RECIPE_DIR/app.svg" "$PREFIX/Menu"
 fi
 
 "$PYTHON" setup.py install || exit 1
