788f407bfd96978639a862f5db3d7a86d1bab8dd
Blaise Thompson <blaise@untzag.com>
2022-09-06 21:11:25 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-mkdir -p "$PREFIX/Menu"
-if [ $OSX_ARCH ]
-then
-    cp "$RECIPE_DIR/menu-osx.json" "$PREFIX/Menu"
-    cp "$RECIPE_DIR/app.icns" "$PREFIX/Menu"
-fi
 
 "$PYTHON" -m pip install . || exit 1
 
